1247041bbbd8ccf6d05b82ce88a7b76d574182ff
aportillo028 <54814904+aportillo028@users.noreply.github.com>
2019-09-23 16:59:52 -0600

    Delete getUrlWeatherByCity.js

--- src/getUrlWeatherByCity.js
+++
@@ -1,7 +0,0 @@
-import {url_base_weather,api_key } from '../constants/api_configurations'
-
-const getUrlWeatherByCity = city =>{
-    return `${url_base_weather}?q=${city}&appid=${api_key}&units=metrics`;
-};
-
-export default getUrlWeatherByCity;
